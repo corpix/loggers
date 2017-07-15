82e27f85c8a48ce6edadfeb7848ba7ecb01756d3
Dmitry Moskowski <me@corpix.ru>
2017-07-15 04:08:46 +0000

    Support logrus creation from configuration

--- target/logrus/logrus.go
+++ target/logrus/logrus.go
@@ -1,10 +1,27 @@
 package logrus
 
 import (
+	"strings"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/corpix/logger"
 )
+
+const (
+	// TextFormatter is a text formatter name for logrus logger.
+	TextFormatter = "text"
+
+	// JSONFormatter is a JSON formatter name for logrus logger.
+	JSONFormatter = "json"
+)
+
+// Config is a logrus configuration which could be
+// customized.
+type Config struct {
+	Level     string
+	Formatter string
+}
 
 // Logrus is a logrus for logger that implements
 // io.Writer interface.
@@ -28,4 +45,34 @@ func (l *Logrus) Level() interface{} {
 // New wraps logrus logger with binding.
 func New(l *logrus.Logger) logger.Logger {
 	return &Logrus{l}
+}
+
+// New creates and wraps new logrus logger with binding.
+func NewFromConfig(c Config) (logger.Logger, error) {
+	var (
+		l   logrus.Level
+		f   logrus.Formatter
+		err error
+	)
+	l, err = logrus.ParseLevel(c.Level)
+	if err != nil {
+		return nil, err
+	}
+
+	switch strings.ToLower(c.Formatter) {
+	case TextFormatter:
+		f = &logrus.TextFormatter{}
+	case JSONFormatter:
+		f = &logrus.JSONFormatter{}
+	case "":
+		f = &logrus.TextFormatter{}
+	default:
+		return nil, NewErrUnknownFormatter(c.Formatter)
+	}
+
+	log := logrus.New()
+	log.Level = l
+	log.Formatter = f
+
+	return New(log), nil
 }
